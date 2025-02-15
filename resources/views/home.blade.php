00be9bd6bc206df50544f354c267218e6907afe0
axelker <axelker@outlook.fr>
2025-02-15 15:50:58 +0100

    feat: adapt section in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,8 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
+        @yield('styles')
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -19,16 +18,20 @@
         @endif
     </head>
     <body class="font-sans antialiased bg-black text-white">
-        <header class="sticky top-0 bg-black w-full py-6 px-8">
+        <header class="sticky top-0 bg-black w-full py-6 px-5">
             <nav class="flex justify-between">
-                <img width="32" height="32" [src]="favicon" alt="logo">
-                <span>EN</span>
+            <img class="logo__img" src="https://www.univerre.ch/wp-content/themes/multi-shop-child/assets/images/logo.svg" alt="logotype">
+            <span>EN</span>
             </nav>
         </header>
 
-        <main class="mt-6">
-            @include('sections.letter')
-            @include('sections.contact')
+        <main class="mt-10 grid grid-cols-1 gap-10">
+            <div class="flex justify-center">
+                @include('sections.letter')
+            </div>
+            <div class="flex justify-center">
+                @include('sections.contact')
+            </div>
         </main>
 
         <footer class="sticky bottom-0 w-full bg-black text-white text-center text-sm py-4 ">
