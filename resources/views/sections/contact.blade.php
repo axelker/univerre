b5b3d0eefc63cf66ad259e66260b70eb8a40cee9
axelker <axelker@outlook.fr>
2025-02-15 15:51:49 +0100

    feat: add base content of contact page

--- resources/views/sections/contact.blade.php
+++ resources/views/sections/contact.blade.php
@@ -1,8 +1,16 @@
-<section id="contact">
-    <h2>Your contact details</h2>
-   <form>
+<section class="max-w-80 text-center" id="contact">
+    <h2 class="text-center">Your contact details</h2>
+   <form class="flex flex-col">
         <input placeholder="Your name">
         <input placeholder="Phone number">
         <input placeholder="Email address">
    </form>
+   <button type="submit"> Submit inquiry</button>
+
+   <div class="submited">
+     <img src="/images/check-circle.png" alt="check-circle">
+     <span> Form Submitted Successfully. Thank you for your interest!
+     <span> Visit <a href="https://www.univerre.ch/" target="_blank" >Univerre.ch</a> </span>
+</div>
+
 </section>
